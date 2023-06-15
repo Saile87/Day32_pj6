90e99943b4d93fcbccc38f8ea428b69aeec9af12
Elias Breitenbach <elias.b@me.com>
2023-06-15 23:12:45 +0200

    Building custom transitions using ViewModifier

--- Day32_pj6/Day32_pj6/ContentView.swift
+++ Day32_pj6/Day32_pj6/ContentView.swift
@@ -7,26 +7,73 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
     
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
+struct ContentView: View {
     @State private var isShowingRed = false
     
     var body: some View {
-        VStack {
-            Button("Tap Me") {
-                withAnimation {
-                    isShowingRed.toggle()
-                }
-            }
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            
             if isShowingRed {
                 Rectangle()
                     .fill(.red)
                     .frame(width: 200, height: 200)
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
             }
         }
     }
 }
+
+
+
+//struct ContentView: View {
+//
+//    @State private var isShowingRed = false
+//
+//    var body: some View {
+////        Showing and hiding views with transitions
+//        VStack {
+//            Button("Tap Me") {
+//                withAnimation {
+//                    isShowingRed.toggle()
+//                }
+//            }
+//            if isShowingRed {
+//                Rectangle()
+//                    .fill(.red)
+//                    .frame(width: 200, height: 200)
+//                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+//            }
+//        }
+//    }
+//}
 
 
 //struct ContentView: View {
