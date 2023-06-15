f8d390b6896befef01c3e1866300cac3a4e4740f
Elias Breitenbach <elias.b@me.com>
2023-06-15 21:48:21 +0200

    Animating gestures

--- Day32_pj6/Day32_pj6/ContentView.swift
+++ Day32_pj6/Day32_pj6/ContentView.swift
@@ -6,24 +6,91 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
-    @State private var enabled = false
+    let letters = Array("Hello, SwiftUI")
+    @State private var enable = false
+    @State private var dragAmount = CGSize.zero
     var body: some View {
-        
-        Button("Tap Me") {
-            enabled.toggle()
-            //        do nothing
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enable ? .red : .blue)
+                    .offset(dragAmount)
+                    .animation(
+                        .default.delay(Double(num) / 20),
+                        value: dragAmount)
+            }
         }
-        .frame(width: 200, height: 200)
-        .background(enabled ? .red : .blue)
-        .animation(nil, value: enabled)
-        .foregroundColor(.white)
-        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
-        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
-        
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    
+                    dragAmount = .zero
+                    enable.toggle()
+                }
+        )
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//struct ContentView: View {
+//    //    Animating gestures
+//    @State private var dragAmount = CGSize.zero
+//    var body: some View {
+//        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+//            .frame(width: 300, height: 200)
+//            .clipShape(RoundedRectangle(cornerRadius: 10))
+//            .offset(dragAmount)
+//            .gesture(
+//                DragGesture()
+//                    .onChanged { dragAmount = $0.translation }
+//                    .onEnded { _ in
+//                        withAnimation {
+//                            dragAmount = .zero }
+//                    }
+//            )
+////            .animation(.spring(), value: dragAmount)
+//    }
+//}
+
+
+    
+//    @State private var enabled = false
+//    var body: some View {
+//
+//        Button("Tap Me") {
+//            enabled.toggle()
+//            //        do nothing
+//        }
+//        .frame(width: 200, height: 200)
+//        .background(enabled ? .red : .blue)
+//        .animation(nil, value: enabled)
+//        .foregroundColor(.white)
+//        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+//        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
+//
+//    }
+//}
         
     
 //    Controlling the animation stack
@@ -47,12 +114,6 @@ struct ContentView: View {
 //
 //    }
 //}
-
-
-    
-    
-    
-    
     
     //    Animating bindings
 //    @State private var animationAmount = 1.0
