c26432c220513156e952bc8732b5eca5041dee4e
Elias Breitenbach <elias.b@me.com>
2023-06-15 22:39:21 +0200

    Showing and hiding views with transitions

--- Day32_pj6/Day32_pj6/ContentView.swift
+++ Day32_pj6/Day32_pj6/ContentView.swift
@@ -6,51 +6,57 @@
 //
 
 import SwiftUI
+
 struct ContentView: View {
-    let letters = Array("Hello, SwiftUI")
-    @State private var enable = false
-    @State private var dragAmount = CGSize.zero
+    
+    @State private var isShowingRed = false
+    
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count) { num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enable ? .red : .blue)
-                    .offset(dragAmount)
-                    .animation(
-                        .default.delay(Double(num) / 20),
-                        value: dragAmount)
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
             }
         }
-        .gesture(
-            DragGesture()
-                .onChanged { dragAmount = $0.translation }
-                .onEnded { _ in
-                    
-                    dragAmount = .zero
-                    enable.toggle()
-                }
-        )
     }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//struct ContentView: View {
+//    let letters = Array("Hello, SwiftUI")
+//    @State private var enable = false
+//    @State private var dragAmount = CGSize.zero
+//    var body: some View {
+//        HStack(spacing: 0) {
+//            ForEach(0..<letters.count) { num in
+//                Text(String(letters[num]))
+//                    .padding(5)
+//                    .font(.title)
+//                    .background(enable ? .red : .blue)
+//                    .offset(dragAmount)
+//                    .animation(
+//                        .default.delay(Double(num) / 20),
+//                        value: dragAmount)
+//            }
+//        }
+//        .gesture(
+//            DragGesture()
+//                .onChanged { dragAmount = $0.translation }
+//                .onEnded { _ in
+//
+//                    dragAmount = .zero
+//                    enable.toggle()
+//                }
+//        )
+//    }
+//}
 
 
 //struct ContentView: View {
